56bbb18982b388667b7f35258d73f152a74fb9f2
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-02-21 15:01:43 +0100

    function touit from API

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // document.addEventListener("DOMContentLoaded", () => {
 //   let today = new Date();
 //   let currentHour = today.getHours();
-  
+
 //   if (currentHour >= 0 && currentHour < 1) {
 //     document.body.style.background = "pink"
 // }
@@ -10,3 +10,17 @@
 //   document.body.style.background ="grey"
 // }
 // });
+
+const touitteur = document.querySelector("#touit");
+const touitContainer = document.querySelector("#touit-container");
+
+const addTouit = () => {
+  const newTouit = document.createElement("div");
+  newTouit.className = "touit";
+
+  const NewTouitContent = document.createElement("p");
+  NewTouitContent.textContent = "Bonjour";
+
+  newTouit.appendChild(NewTouitContent);
+  touitContainer.appendChild(newTouit);
+};
