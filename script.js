b4ad33f180d206dccf7c4b106a0a375fad100a7d
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-02-21 14:12:13 +0100

    touitter html css

---
+++ script.js
@@ -0,0 +1,12 @@
+// const bodyPage = document.querySelector("body");
+// document.addEventListener("DOMContentLoaded", () => {
+//   let today = new Date();
+//   let currentHour = today.getHours();
+  
+//   if (currentHour >= 0 && currentHour < 1) {
+//     document.body.style.background = "pink"
+// }
+// else {
+//   document.body.style.background ="grey"
+// }
+// });
