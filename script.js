b148d5983d9793c55f73041b50a0b3badd4139ae
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-02-21 17:26:25 +0100

    fuction + console.log done + css modif

--- script.js
+++ script.js
@@ -15,12 +15,22 @@ const touitteur = document.querySelector("#touit");
 const touitContainer = document.querySelector("#touit-container");
 
 const addTouit = () => {
-  const newTouit = document.createElement("div");
+  const nextTouit = document.createElement("div");
   newTouit.className = "touit";
 
   const NewTouitContent = document.createElement("p");
   NewTouitContent.textContent = "Bonjour";
 
-  newTouit.appendChild(NewTouitContent);
+  nextTouit.appendChild(NewTouitContent);
   touitContainer.appendChild(newTouit);
 };
+
+const sendTouit = document.querySelector("#message");
+const user = document.querySelector("#pseudo");
+const btnForm = document.querySelector("#form-btn");
+
+btnForm.addEventListener("click", () => {
+  const userName = user.value + " : ";
+  const touitNew = sendTouit.value;
+  console.log(userName, touitNew);
+});
